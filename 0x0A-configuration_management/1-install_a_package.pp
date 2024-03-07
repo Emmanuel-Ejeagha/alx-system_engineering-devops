01569b66e7ee9bb60bd3a53e4a9a74770cb4f14e
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-07 22:04:04 +0000

    web-01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# 1-install_a_package.pp
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0"',
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
